77fa638283bd9b5d42dcc3ce5d7d590144d2e231
hachondapargo <hachonda@pargo.co.za>
2018-05-22 13:49:34 +0200

    Add files via upload

---
+++ inc/pargo-tracking-optin.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+		var $html = $('html');
+
+		$('.fs-permissions .fs-trigger').on('click', function () {
+			$('.fs-permissions').toggleClass('fs-open');
+
+			return true;
+		});
+
+		
+	});
